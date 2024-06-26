79cbe97cc0011e48dd1b5db283f46a3e87c1c92f
AlbinQestaj <albinqestaj@gmail.com>
2024-06-26 12:10:39 +0200

    editing the port to 5000

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // adding my AWS EC2 public IP
-  baseURL: 'http://54.221.17.177:4000/api',
+  baseURL: 'http://54.221.17.177:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
